346ff14e3dd67bafe0962da9f78ac29705203e16
Keops2002 <83623787+KeopsA02@users.noreply.github.com>
2023-06-27 17:39:57 -0600

    led_and_push_button.ino

--- Course/Practice2/led_and_push_button.ino
+++ Course/Practice2/led_and_push_button.ino
@@ -1,4 +1,5 @@
-/* Code by Servicios Universales Keops.
+/* 
+* Code by Servicios Universales Keops.
 * You should do the circuit showed in the image that it's in this folder.
 */
 
@@ -11,6 +12,7 @@ void loop() {
   // The if sentence read a sentence, and if the sentence is true, return a 1 (HIGH) and if the sentence is false return a 0 (FALSE)
   // If is true, it does the action in keys of "if", otherwise, it does the action in keys of "else"
   // digitalRead() read the pin and save the value (0 or 1).
+  
   if(digitalRead(13) == HIGH){
     digitalWrite(12, HIGH);
   }else{
